f700f73c94c72e77a7801a42dfe0c62a7d99f795
Thiago Ferreira <131627512+TenTemujin@users.noreply.github.com>
2025-04-24 14:39:09 -0300

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   url: 'https://tentemujin.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl = /SHARC_Documentation_test/,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
